f543c99efd5658a4c72132c597b0724ccd153664
zhdhfhd33 <zhdhfhd33@naver.com>
2022-07-30 16:52:12 +0900

    리눅스 테스트 케이스 수정

--- src/test/java/pnu/problemsolver/myorder/repository/DemandRepositoryTest.java
+++ src/test/java/pnu/problemsolver/myorder/repository/DemandRepositoryTest.java
@@ -12,62 +12,67 @@ import pnu.problemsolver.myorder.domain.constant.DemandStatus;
 @Transactional
 @Commit
 class DemandRepositoryTest {
-
-    @Autowired
-    CustomerRepository customerRepository;
-
-    @Autowired
-    StoreRepository storeRepository;
-
-    @Autowired
-    DemandRepository demandRepository;
-
-    @Autowired
-    CakeRepositroy cakeRepositroy;
-
-    @Test
-    public void save생성_수정_시간차이확인() {
-        Customer customer = Customer.builder().build();
+	
+	@Autowired
+	CustomerRepository customerRepository;
+	
+	@Autowired
+	StoreRepository storeRepository;
+	
+	@Autowired
+	DemandRepository demandRepository;
+	
+	@Autowired
+	CakeRepositroy cakeRepositroy;
+	
+	@Test
+	public void save생성_수정_시간차이확인() {
+		Store s = Store.builder()
+				.build();
+		storeRepository.save(s);
+		
+		
+		Customer customer = Customer.builder().build();
 //        System.out.println("유유아이디"+customer.getUuid());
-        customerRepository.save(customer);
-        System.out.println(customer);
-
-        Cake cake = Cake.builder()
-                .name("cakeName")
-                .minPrice(10000)
-                .description("설명")
-                .build();
-        cakeRepositroy.save(cake);
-        cake.setMinPrice(2);
-
-        Cake newCake = Cake.builder()
-                .uuid(cake.getUuid())
-                .minPrice(3)
-                .name("새로만듦")
-                .build();
-
-        cakeRepositroy.save(newCake);
-
-        System.out.println(cake);
-
-
-        Demand demand = Demand.builder()
-                .customer(customer)
-                .cake(cake)
-                .status(DemandStatus.WAITING)
-                .build();
-
-        demandRepository.save(demand);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        demand.acceptDemand();//주문수락
-        demandRepository.save(demand);
-    }
-
-
-
-
+		customerRepository.save(customer);
+		System.out.println(customer);
+		
+		Cake cake = Cake.builder()
+				.store(s)
+				.name("cakeName")
+				.minPrice(10000)
+				.description("설명")
+				.build();
+		cakeRepositroy.save(cake);
+		cake.setMinPrice(2);
+		
+		Cake newCake = Cake.builder()
+				.uuid(cake.getUuid())
+				.store(s)
+				.minPrice(3)
+				.name("새로만듦")
+				.build();
+		
+		cakeRepositroy.save(newCake);
+		
+		System.out.println(cake);
+		
+		
+		Demand demand = Demand.builder()
+				.customer(customer)
+				.cake(cake)
+				.status(DemandStatus.WAITING)
+				.build();
+		
+		demandRepository.save(demand);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+		demand.acceptDemand();//주문수락
+		demandRepository.save(demand);
+	}
+	
+	
 }
